adfdcdf7ec11ca5ffaf359a8a45feab1b02c9766
Cassandra Torske <cassandra.torske@gmail.com>
2017-05-19 13:29:26 -0600

    feng shui

--- public/app.module.js
+++ public/app.module.js
@@ -2,13 +2,13 @@
 
   angular.module('app', ['ui.router'])
     .component('home', {
-      templateUrl: '/home.html',
+      templateUrl: '/home/home.html',
       controller: "HomeController"
     }).component('about', {
-      templateUrl: '/about.html',
+      templateUrl: '/about/about.html',
       controller: "AboutController"
     }).component('contact', {
-      templateUrl: 'contact.html',
+      templateUrl: '/contact/contact.html',
       controller: "ContactController"
     })
 }())
