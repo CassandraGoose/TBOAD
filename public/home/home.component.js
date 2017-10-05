2977aaaad01214991d452aabee8187c56a87f585
Cassandra Torske <cassandra.torske@gmail.com>
2017-10-05 14:03:29 -0600

    update home

--- public/home/home.component.js
+++ public/home/home.component.js
@@ -7,18 +7,14 @@ function HomeController($http, $stateParams, $state) {
   vm.messageAdd = messageAdd
 
   vm.$onInit = function() {
-
-    console.log("hello from home component");
   }
 
   function messageAdd() {
-      console.log("message add function");
       $http.post("http://tboadpodcast.herokuapp.com/messages", {
           name: vm.message.name,
           text: vm.message.text
         })
         .then(function(response) {
-          console.log('done!');
         })
 
     }
